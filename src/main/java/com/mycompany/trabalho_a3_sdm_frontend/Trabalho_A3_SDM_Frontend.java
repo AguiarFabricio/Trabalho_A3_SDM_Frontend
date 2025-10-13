5aab52059464ea2d6503794fb00a294cd68ae13a
AguiarFabricio <aguiar2006@gmail.com>
2025-10-13 09:04:10 -0300

    feat: commit

---
+++ src/main/java/com/mycompany/trabalho_a3_sdm_frontend/Trabalho_A3_SDM_Frontend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho_a3_sdm_frontend;
+
+/**
+ *
+ * @author Fabricio de Aguiar
+ */
+public class Trabalho_A3_SDM_Frontend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
