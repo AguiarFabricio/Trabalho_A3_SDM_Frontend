441a8f21e8def1882826c6f0b30a74c5937f96bc
Luiz Gustavo <pereira.since2004@gmail.com>
2025-10-28 21:17:38 -0300

    Teste

--- src/main/java/com/mycompany/trabalho_a3_sdm_frontend/Trabalho_A3_SDM_Frontend.java
+++ src/main/java/com/mycompany/trabalho_a3_sdm_frontend/Trabalho_A3_SDM_Frontend.java
@@ -11,6 +11,6 @@ package com.mycompany.trabalho_a3_sdm_frontend;
 public class Trabalho_A3_SDM_Frontend {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!!");
     }
 }
