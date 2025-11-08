ed58e487c24e966b39f8749d933b26da82d89fe4
Luiz Gustavo <pereira.since2004@gmail.com>
2025-11-08 11:42:30 -0300

    fix: ajustado layout interface principal

--- src/main/java/view/frmprincipal/FrmPrincipal.java
+++ src/main/java/view/frmprincipal/FrmPrincipal.java
@@ -7,6 +7,7 @@ package view.frmprincipal;
 import view.frmcategoria.FrmCategoria;
 import view.frmproduto.FrmGerenciarProdutos;
 
+
 /**
  *
  * @author luiz
@@ -96,8 +97,6 @@ public class FrmPrincipal extends javax.swing.JFrame {
         MovimentarEst = new javax.swing.JLabel();
         Relatorios = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
 
@@ -120,7 +119,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Gerenciador");
+        jLabel1.setText("Menu");
 
         GerenciarCat.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         GerenciarCat.setForeground(new java.awt.Color(255, 255, 255));
@@ -154,53 +153,33 @@ public class FrmPrincipal extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(0, 0, 51));
-        jLabel2.setText("Produtos Ativos");
-
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(0, 0, 51));
-        jLabel3.setText("Categorias Ativas:");
-
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 28)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(0, 0, 51));
-        jLabel4.setText("0");
+        jLabel4.setText("Sistema de Gerenciamento de Estoque");
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(0, 0, 51));
-        jLabel5.setText("0");
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel5.setText("© 2025 A3 Sistemas Distribuidos. Luiz, Mario e Fabricio. Todos os direitos reservados.");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(152, 152, 152)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(112, 112, 112)
-                        .addComponent(jLabel3))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(57, 57, 57)
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel5)
-                        .addGap(66, 66, 66)))
-                .addGap(135, 135, 135))
+                .addGap(96, 96, 96)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
+                .addContainerGap(149, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(223, 223, 223)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(258, Short.MAX_VALUE)
+                .addComponent(jLabel4)
+                .addGap(222, 222, 222)
+                .addComponent(jLabel5)
+                .addGap(16, 16, 16))
         );
 
         javax.swing.GroupLayout GuiaGerenciadorLayout = new javax.swing.GroupLayout(GuiaGerenciador);
@@ -222,7 +201,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
                         .addGap(32, 32, 32)
                         .addComponent(Relatorios))
                     .addGroup(GuiaGerenciadorLayout.createSequentialGroup()
-                        .addGap(46, 46, 46)
+                        .addGap(75, 75, 75)
                         .addComponent(jLabel1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -240,7 +219,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
                 .addComponent(MovimentarEst)
                 .addGap(18, 18, 18)
                 .addComponent(Relatorios)
-                .addContainerGap(247, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
@@ -280,7 +259,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
         } catch (Exception e) {
             e.printStackTrace();
             javax.swing.JOptionPane.showMessageDialog(this,
-                "Erro ao abrir a tela de Categorias: " + e.getMessage());
+                    "Erro ao abrir a tela de Categorias: " + e.getMessage());
         }
     }//GEN-LAST:event_GerenciarProdMouseClicked
 
@@ -292,7 +271,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
         } catch (Exception e) {
             e.printStackTrace();
             javax.swing.JOptionPane.showMessageDialog(this,
-                "Erro ao abrir a tela de Categorias: " + e.getMessage());
+                    "Erro ao abrir a tela de Categorias: " + e.getMessage());
         }
     }//GEN-LAST:event_GerenciarCatMouseClicked
 
@@ -330,8 +309,6 @@ public class FrmPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel Relatorios;
     private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
